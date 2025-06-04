8dc6c88170e5238adb5c34540c21625bc08901fc
Evil_Little_Pony <57661218+AnnMaleneva@users.noreply.github.com>
2025-06-04 12:29:48 +0400

    Add files via upload

---
+++ TaskService/Context/TaskDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskService.Models;
+
+namespace TaskService.Context
+{
+    public class TaskDbContext : DbContext
+    {
+        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options) { }
+    }
+}
